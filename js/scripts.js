22fe65046f4c692f626bd6b45a2d61fe3a1619f3
Kayla Ondracek and Stephanie Faber <kayla@>
2018-01-10 09:29:32 -0800

    add form to take user input and jquery to capitalize input and put in a list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
+  $("#shoutingForm").submit(function(event) {
+      var lowercaseInput = $("input#userInput").val();
+
+      $(".shouting").append("<li>"+lowercaseInput.toUpperCase()+"!!!"+"</li>" );
+
+
+    event.preventDefault();
+  });
 
 
 });
